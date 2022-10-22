0fdaa58f2276de4c49663d9fb6fb574d9008c745
DDlb <1032191254@qq.com>
2022-10-22 12:49:46 +0800

    init

---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<script setup>
+
+</script>
+
+<template>
+
+</template>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+}
+</style>
